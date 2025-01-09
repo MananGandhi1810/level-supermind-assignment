1df8db6f8596707d2edefb2e779bc1d92a09d1f7
Naitik Lodha <naitik.lodha139@nmims.edu.in>
2025-01-09 21:15:49 +0530

    fix:build error

--- Frontend/app/insights/page.js
+++ Frontend/app/insights/page.js
@@ -1,3 +1,4 @@
+"use client";
 import InstagramCharts from "../components/charts";
 import ChatInterface from "../components/chat";
 import {
@@ -5,18 +6,21 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <div className="flex h-screen overflow-hidden custom-scrollbar">
-      <div className="flex-grow overflow-auto p-4">
-        <div className="max-w-5xl mx-auto">
-          <InstagramCharts />
+    <Suspense>
+      <div className="flex h-screen overflow-hidden custom-scrollbar">
+        <div className="flex-grow overflow-auto p-4">
+          <div className="max-w-5xl mx-auto">
+            <InstagramCharts />
+          </div>
+        </div>
+        <div className="hidden md:block w-[25%] flex-shrink-0">
+          <ChatInterface />
         </div>
       </div>
-      <div className="hidden md:block w-[25%] flex-shrink-0">
-        <ChatInterface />
-      </div>
-    </div>
+    </Suspense>
   );
 }
